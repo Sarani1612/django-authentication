22629c0d41198139aeb18a96d97e87a76cc1f499
Sarani1612 <sara.larsen1612@gmail.com>
2020-03-08 11:12:16 +0000

    added registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -10,8 +10,7 @@ def index(request):
     return render(request, 'index.html')
 
 
-
-@login_required  # this ensures users that are NOT logged in can't get access to the logout page which could cause bugs 
+@login_required  # this ensures users that are NOT logged in can't get access to the logout page which could cause bugs
 def logout(request):
     '''Log the user out'''
     auth.logout(request)
@@ -38,3 +37,30 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+
+
+def registration(request):
+    '''render the registration page'''
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == 'POST':
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered!")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+
+    else:
+        registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
